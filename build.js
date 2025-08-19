e9d880a4b4056f6d68e90dd82e56fbb0ef48330c
Brennan Vincent <brennan@umanwizard.com>
2025-08-19 14:47:16 -0700

    build on all platforms

--- build.js
+++ build.js
@@ -3,5 +3,5 @@ const { execSync } = require('child_process');
 if (process.platform === 'linux') {
   execSync('node-gyp rebuild --verbose', { stdio: 'inherit' });
 } else {
-  console.log('Skipping native addon build on this platform:', process.platform);
+  execSync('node-gyp rebuild --verbose', { stdio: 'inherit' });
 }
