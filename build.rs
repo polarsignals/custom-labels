a662353245ba65d6e64cbb2520aa584257ece34f
Brennan Vincent <brennan@umanwizard.com>
2026-04-01 14:30:26 -0700

    rebuild if build.rs changed

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=src/customlabels.cpp");
+    println!("cargo:rerun-if-changed=./build.rs");
     println!("cargo:rerun-if-changed=src/customlabels.h");
     println!("cargo:rerun-if-changed=./dlist");
 
