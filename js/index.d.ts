5146a120baa020d082e5da46d7c1d4065835bdcf
Scott Gerring <scott@scottgerring.com>
2026-04-07 18:17:16 +0200

    Remove unused files

--- js/index.d.ts
+++
@@ -1,5 +0,0 @@
-export type LabelValue = string | boolean | number | null | undefined;
-
-export function withLabels<T>(f: () => T, ...kvs: LabelValue[]): T;
-
-export function curLabels(): Record<string, string> | undefined;
